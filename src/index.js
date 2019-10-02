cdbf7de0297a4f87b7bae298520104e0160f003f
iskanderror <iskanderror@gmail.com>
2019-10-03 00:33:47 +0500

    feat: add first and last bracket check

--- src/index.js
+++ src/index.js
@@ -5,4 +5,21 @@ module.exports = function check(str, bracketsConfig) {
     return false;
   }
 
+  let halfLength = str.length/2;
+  for (let i = 0; i<halfLength; i++) {
+    let sample = [ str[i],str[str.length-1-i] ];
+    if ( !checkExternalBrackets(sample,bracketsConfig) ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkExternalBrackets(sample, bracketsConfig){
+  for (let i=0; i< bracketsConfig.length; i++) {
+    if ( sample[0] === bracketsConfig[i][0] && sample[1] === bracketsConfig[i][1] ) {
+      return true;
+    }
+  }
+  return false;
 }
